f41961d4c75be1e94e5f67cb7e2324ec7332e156
Faith M. Roberts <faithroberts708@gmail.com>
2023-02-18 04:40:20 +0100

    Adding my whitelist dapp project

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
+
+//Whitelist Contract Address: 0x304f77d19F05F90E75dA45ac70d47Ce93e9dA19f
